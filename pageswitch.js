209260c09fb6f0a71e4a46fc4419fbabfc6cd3ec
guoyanhao <769151351@qq.com>
2016-09-01 16:50:26 +0800

    first commit

---
+++ pageswitch.js
@@ -0,0 +1,70 @@
+(function($){
+	var privateFun = function(){
+		//
+	}
+	$.fn.PageSwitch = function(options){
+		var PageSwitch = (function(){
+			function PageSwitch(element, options){
+				this.settings = $.extend(true, $.fn.PageSwitch.default, options||{});
+				this.element = element;
+				this.init();
+			}
+			PageSwitch.prototype = {
+				init: function(){
+
+				}
+			}
+			return PageSwitch;
+		})();
+		return this.each(function(){
+			var me = $(this),
+				instance = me.data("PageSwitch");
+
+			if(!instance) {
+				instance = new PageSwitch(me, options);
+				me.data("PageSwitch", instance);
+			}
+			if($.type(options) === "string") return instance[options]();
+
+
+		});
+	}
+	$.fn.PageSwitch.default = {
+		selectors : {
+			sections:".sections",
+			section:".section",
+			page:".pages",
+			active:".active"
+		},
+		index : 0,
+		easing : "ease",
+		duration : 500,
+		loop : false,
+		pagination : true,
+		keyboard : true,
+		direction : "vertical",
+		callback : ""
+	}
+
+	$(function(){
+		$("[data-PageSwitch]").PageSwitch();
+	})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+})(jQuery);
+
